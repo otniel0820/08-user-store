aff5ea14f92ed648f021b61f4280af53147032fc
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-02-16 03:11:45 +0100

    create custom errors

---
+++ src/domain/errors/custom.errors.ts
@@ -0,0 +1,28 @@
+
+
+export class CustomErrors extends Error {
+
+    constructor(
+        public readonly statusCode: number, 
+        public readonly message: string, 
+    ){
+        super(message)
+    }
+
+    static badRequest(message: string){
+        return new CustomErrors(400, message)
+    }
+    static unAuthorize(message: string){
+        return new CustomErrors(401, message)
+    }
+    static forbidden(message: string){
+        return new CustomErrors(403, message)
+    }
+    static notFound(message: string){
+        return new CustomErrors(404, message)
+    }
+    static internalServer(message: string){
+        return new CustomErrors(500, message)
+    }
+
+}
