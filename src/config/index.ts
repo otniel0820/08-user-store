63c801752699aecfa801cfeb384afe388612256c
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-03-24 14:46:54 +0100

    assign uuid to the image as name and validate the extension

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,4 @@ export * from './envs';
 export * from './jwt.adapter';
 export * from './regular-exp';
 export * from './validator'
+export * from './uuid.adapter'
--- src/presentation/services/file-upload-service.ts
+++ src/presentation/services/file-upload-service.ts
@@ -1,11 +1,15 @@
 import path from "path";
 import fs from "fs";
 import { UploadedFile } from "express-fileupload";
+import { Uuid } from "../../config";
+import { CustomErrors } from "../../domain";
 
 
 
 export class FileUploadService{
-    constructor(){}
+    constructor(
+        private readonly uuid = Uuid.v4,
+    ){}
 
     private checkFolder(folderPath: string){
         if (!fs.existsSync(folderPath)) {
@@ -19,11 +23,15 @@ export class FileUploadService{
         validEstensions: string[]=['jpg', 'png', 'jpeg', 'gif'],
     ){
         try {
-            const fileExtensions = file.mimetype.split('/').at(1) // separamos el mimetype por el slash y tomamos su segunda posicion para saber el tipo de archivo
+            const fileExtensions = file.mimetype.split('/').at(1) ?? ''// separamos el mimetype por el slash y tomamos su segunda posicion para saber el tipo de archivo
+            if(!validEstensions.includes(fileExtensions)){
+                throw CustomErrors.badRequest(`Invalid extension: ${fileExtensions}, valid ones${validEstensions} `); // haciendo la validacion de las extensiones si no es ninguna a las que tenemos definidas dara un error 
+            }
             const destination = path.resolve(__dirname, '../../../', folder)// aqui tomamos todo el path de nuestro ordenador no solo la ruta en la que lo almacenaremos en el proyecto
             this.checkFolder(destination)
-            file.mv(destination + `/mi-imagen.${fileExtensions}`)
-            
+            const fileName = `${this.uuid()}.${fileExtensions}`
+            file.mv(`${destination}/${fileName}`)
+            return {fileName}
         } catch (error) {
             console.log(error);
             
