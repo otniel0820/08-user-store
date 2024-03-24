04637da9482d6b0e0198d2462cf84e199c6545bd
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-03-24 17:42:23 +0100

    return image

---
+++ src/presentation/images/controller.ts
@@ -0,0 +1,20 @@
+
+import { Request, Response } from "express";
+import path from "path";
+import fs from 'fs';
+
+
+export class ImageController{
+    constructor(){}
+
+    getImage=(req:Request,res:Response)=>{
+        const {type= '', img = ''}= req.params;
+        const imagePath = path.resolve(__dirname, `../../../uploads/${type}/${img}`);
+
+        if (!fs.existsSync(imagePath)) {
+            return  res.status(404).send("Image Not Found");
+        }
+        
+        res.sendFile(imagePath);
+    }
+}
